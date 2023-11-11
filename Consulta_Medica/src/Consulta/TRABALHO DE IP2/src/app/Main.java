c1883b59809d863c1630f355fb5ec75531c2d956
rodrigo052003 <147418208+rodrigo052003@users.noreply.github.com>
2023-11-10 23:30:47 -0300

    Add files via upload

---
+++ Consulta_Medica/src/Consulta/TRABALHO DE IP2/src/app/Main.java
@@ -0,0 +1,20 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        enfermeirasHospitalar enfermeira = new enfermeirasHospitalar();
+
+        //enfermeirasHospitalar Enfermeira = new enfermeirasHospitalar(Cod: "1234", especialidade: "Obstreta", procedimento: "Cirurgia");
+        //Enfermeira.Cod = "1234";
+        //Enfermeira.especialidade = "Obstreta";
+        //Enfermmeira.procedimento = "Cirurgico";
+
+
+
+
+
+
+        }
+    }

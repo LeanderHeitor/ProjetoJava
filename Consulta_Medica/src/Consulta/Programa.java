d1ff57e1bf7443e1e9cfb932d3e369ded716bdfb
Antônio Guedes <146470610+tonigds1@users.noreply.github.com>
2023-12-06 17:51:24 -0300

    Update Programa.java

--- Consulta_Medica/src/Consulta/Programa.java
+++ Consulta_Medica/src/Consulta/Programa.java
@@ -1,12 +1,76 @@
 package Consulta;
 import Consulta.Negocios.Beans.Paciente;
+import Consulta.Negocios.ControladorMedico;
+import Consulta.Negocios.Beans.AgendaSaude;
 import Consulta.Negocios.Beans.ConsultaInfo;
+import Consulta.Negocios.Beans.Medico;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programa {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //Criando 12 Médicos
+        Medico medico1 = new Medico(1, "João Silva", "Cardiologia");
+        Medico medico2 = new Medico(2, "Maria Oliveira", "Pediatria");
+        Medico medico3 = new Medico(3, "Carlos Santos", "Ortopedia");
+        Medico medico4 = new Medico(4, "Ana Costa", "Ginecologia");
+        Medico medico5 = new Medico(5, "Pedro Almeida", "Dermatologia");
+        Medico medico6 = new Medico(6, "Sofia Pereira", "Psiquiatria");
+        Medico medico7 = new Medico(7, "Miguel Ferreira", "Otorrinolaringologia");
+        Medico medico8 = new Medico(8, "Beatriz Ramos", "Endocrinologia");
+        Medico medico9 = new Medico(9, "Ricardo Gonçalves", "Neurologia");
+        Medico medico10 = new Medico(10, "Isabel Marques", "Oncologia");
+        Medico medico11 = new Medico(11, "Tiago Lopes", "Urologia");
+        Medico medico12 = new Medico(12, "Laura Fernandes", "Oftalmologia");
+        
+        ControladorMedico controlador = ControladorMedico.getInstance();
+        List<Medico> listaDeMedicos = new ArrayList<>();
+        
+        // Adicione os médicos à lista
+        listaDeMedicos.add(medico1);
+        listaDeMedicos.add(medico2);
+        listaDeMedicos.add(medico3);
+        listaDeMedicos.add(medico4);
+        listaDeMedicos.add(medico5);
+        listaDeMedicos.add(medico6);
+        listaDeMedicos.add(medico7);
+        listaDeMedicos.add(medico8);
+        listaDeMedicos.add(medico9);
+        listaDeMedicos.add(medico10);
+        listaDeMedicos.add(medico11);
+        listaDeMedicos.add(medico12);
+        // Iterar sobre a lista e cadastrar os médicos
+        for (Medico medico : listaDeMedicos) {
+            controlador.cadastrarMedico(medico);
+        }
+
+        //Criado a agenda saúde para cada médico
+        LocalDate dataExemplo = LocalDate.of(2023, 10, 15); // Exemplo de data
+        LocalTime horaExemplo = LocalTime.of(14, 30); // Exemplo de horário
+        String tipoConsultaExemplo = "Consulta de rotina"; // Exemplo de tipo de consulta
+        int numeroConsultorioExemplo = 3; // Exemplo de número do consultório
+
+        AgendaSaude agendaMedico1 = new AgendaSaude(1, "João Silva", "Cardiologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico2 = new AgendaSaude(2, "Maria Oliveira", "Pediatria", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico3 = new AgendaSaude(3, "Carlos Santos", "Ortopedia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico4 = new AgendaSaude(4, "Ana Costa", "Ginecologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico5 = new AgendaSaude(5, "Pedro Almeida", "Dermatologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico6 = new AgendaSaude(6, "Sofia Pereira", "Psiquiatria", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico7 = new AgendaSaude(7, "Miguel Ferreira", "Otorrinolaringologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico8 = new AgendaSaude(8, "Beatriz Ramos", "Endocrinologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico9 = new AgendaSaude(9, "Ricardo Gonçalves", "Neurologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico10 = new AgendaSaude(10, "Isabel Marques", "Oncologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico11 = new AgendaSaude( 11, "Tiago Lopes", "Urologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+        AgendaSaude agendaMedico12 = new AgendaSaude(12, "Laura Fernandes", "Oftalmologia", dataExemplo, horaExemplo, tipoConsultaExemplo, numeroConsultorioExemplo);
+
+        //Adicionar as agendasaúdes para o reposiitório
 
         // Exemplo de uso da classe Paciente
         Paciente paciente = new Paciente(1, "João", "01/01/1990", "Plano Ouro", "01/01/2024");

c1906ba1241cd2b74ae335f5748a9b42d0818509
rodrigo052003 <147418208+rodrigo052003@users.noreply.github.com>
2023-12-05 22:32:46 -0300

    Add files via upload

---
+++ Consulta_Medica/bin/Consulta/Negocios/Beans/Enfermeira.java
@@ -0,0 +1,39 @@
+public class Enfermeira {
+    // Atributos
+    private String especialidade;
+    private int codigo;
+
+    // Construtor
+    public Enfermeira(String especialidade, int codigo) {
+        this.especialidade = especialidade;
+        this.codigo = codigo;
+    }
+
+    // Método para realizar um procedimento
+    public void realizarProcedimento(String nomePaciente, String procedimento) {
+        System.out.println("Enfermeira está realizando o seqguinte procedimento: " + procedimento +
+                " para o paciente " + nomePaciente);
+    }
+
+    // Métodos de acesso (getters e setters)
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    // Método para exibir informações da enfermeira
+    public void exibirInformacoes() {
+        System.out.println("     *Enfermeira* \nEspecialidade: " + especialidade + "\nCódigo: " + codigo);
+    }
+}

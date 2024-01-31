5a57f040194fcb3b75438cc1a3ca631470ff4b6b
rodrigo052003 <147418208+rodrigo052003@users.noreply.github.com>
2024-01-31 16:53:36 -0300

    Update Programa.java

--- Consulta_Medica/src/Consulta/Programa.java
+++ Consulta_Medica/src/Consulta/Programa.java
@@ -95,6 +95,37 @@ public class Programa {
         listadeAgendaSaudes.add(agendaMedico);
         }
         
+        // Criar uma lista para armazenar enfermeiros
+        List<Enfermeiro> enfermeiroo = new ArrayList<>();
+
+        // Criar uma lista para armazenar pacientes
+        List<Paciente> pacientes = new ArrayList<>();
+        pacientes.add(new Paciente());
+        pacientes.add(new Paciente());
+        pacientes.add(new Paciente());
+        pacientes.add(new Paciente());
+        pacientes.add(new Paciente());
+        pacientes.add(new Paciente());
+
+        // Exemplo de enfermeiros
+        enfermeiroo.add(new Enfermeiro("Cardiologia", 2311));
+        enfermeiroo.add(new Enfermeiro("Pediatria", 4536));
+        enfermeiroo.add(new Enfermeiro("Ortopedia", 0475));
+        enfermeiroo.add(new Enfermeiro("Ginecologia", 8162));
+        enfermeiroo.add(new Enfermeiro("Cirurgia", 5948));
+        enfermeiroo.add(new Enfermeiro("Oncologia", 7362));
+
+        // Iterar sobre a lista de enfermeiros e associar um enfermeiro a cada paciente
+        for (int i = 0; i < enfermeiros.size(); i++) {
+            Enfermeiro enfermeiro = enfermeiros.get(i);
+            Paciente paciente = pacientes.get(i);
+
+            enfermeiro.exibirInformacoes();
+            enfermeiro.realizarProcedimento(paciente.nome, "Procedimento específico para " + enfermeiro.getEspecialidade());
+            System.out.println();
+        }
+    }
+}
         // Exemplo de uso da classe Paciente
         Paciente paciente = new Paciente(1, "João", "01/01/1990", "Plano Ouro", "01/01/2024");
 

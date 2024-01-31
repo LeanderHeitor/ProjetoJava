55b15d97374cd33224ca6eb2691c48f82dd3d96d
rodrigo052003 <147418208+rodrigo052003@users.noreply.github.com>
2024-01-31 17:01:29 -0300

    Update RepositorioMedico.java

--- Consulta_Medica/src/Consulta/Dados/RepositorioMedico.java
+++ Consulta_Medica/src/Consulta/Dados/RepositorioMedico.java
@@ -1,35 +1,54 @@
-package Consulta.Dados;
-import Consulta.Negocios.Beans.Medico;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class RepositorioMedico {
-    private List <Medico> medicos;
+public class EnfermeiroRepository {
 
-    public RepositorioMedico() {
-        medicos = new ArrayList<>();
+    private List<Enfermeiro> enfermeiros;
+
+    public EnfermeiroRepository() {
+        this.enfermeiros = new ArrayList<>();
     }
 
-    public void addMedico(Medico medico){
-        medicos.add(medico);
+    public void adicionarEnfermeiro(Enfermeiro enfermeiro) {
+        enfermeiro.setCodigo(enfermeiros.size() + 1);
+        enfermeiros.add(enfermeiro);
     }
 
-    public void removerMedico(int codigo){
-        for(Medico medico : medicos){
-            if(medico.getCodigoDoutor() == codigo){
-                medicos.remove(medico);
+    public void removerEnfermeiro(int codigoEnfermeiro) {
+        Iterator<Enfermeiro> iterator = enfermeiros.iterator();
+        while (iterator.hasNext()) {
+            Enfermeiro enfermeiro = iterator.next();
+            if (enfermeiro.getCodigo() == codigoEnfermeiro) {
+                iterator.remove();
                 break;
             }
         }
     }
 
-    public Medico buscarMedico (int codigo){
-        for(Medico medico: medicos){
-            if(medico.getCodigoDoutor() == codigo){
-                return medico;
-            }
-        }
-        return null;
+    public List<Enfermeiro> listarEnfermeiros() {
+        return new ArrayList<>(enfermeiros);
     }
-  
+
+    public static void main(String[] args) {
+        EnfermeiroRepository repository = new EnfermeiroRepository();
+
+        // Exemplo de enfermeiros
+        repository.adicionarEnfermeiro(new Enfermeiro("Cardiologia", 0));
+        repository.adicionarEnfermeiro(new Enfermeiro("Pediatria", 0));
+        repository.adicionarEnfermeiro(new Enfermeiro("Ortopedia", 0));
+        repository.adicionarEnfermeiro(new Enfermeiro("Ginecologia", 0));
+        repository.adicionarEnfermeiro(new Enfermeiro("Cirurgia", 0));
+        repository.adicionarEnfermeiro(new Enfermeiro("Oncologia", 0));
+
+        System.out.println("Enfermeiros cadastrados:");
+        repository.listarEnfermeiros().forEach(Enfermeiro::exibirInformacoes);
+
+        // Remover um enfermeiro (substitua o código pelo desejado)
+        repository.removerEnfermeiro(1);
+
+        System.out.println("\nEnfermeiros após remoção:");
+        repository.listarEnfermeiros().forEach(Enfermeiro::exibirInformacoes);
+    }
 }
+

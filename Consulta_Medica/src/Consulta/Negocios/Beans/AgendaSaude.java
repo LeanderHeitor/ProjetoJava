9085329c29beb4e6cad434672d7819cd442a60e6
guiribas87 <143145926+guiribas87@users.noreply.github.com>
2024-02-01 02:12:25 -0300

    Update AgendaSaude.java
    - Criação da variável "gravidade" para se adaptar ao método da classe Recepcionista.

--- Consulta_Medica/src/Consulta/Negocios/Beans/AgendaSaude.java
+++ Consulta_Medica/src/Consulta/Negocios/Beans/AgendaSaude.java
@@ -1,25 +1,34 @@
-package Consulta.Negocios.Beans;
+ackage Consulta.Negocios.Beans;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class AgendaSaude extends Medico {
-    //Atributos
-	private LocalDate data;
-    private LocalTime hora;
-    private String tipoConsulta;
-    private int numeroConsultorio;
+public class AgendaSaude {
+	   // Atributos
+	   private Medico medico;
+	   private LocalDate data;
+	   private LocalTime hora;
+	   private String tipoConsulta;
+	   private int numeroConsultorio;
+	   private String gravidade;
+	   
 
-	//Construtor
-    public AgendaSaude(int id, String nome, String especialidade, LocalDate data, LocalTime hora, String tipoConsulta, int numeroConsultorio) {
-        super(id, nome, especialidade);
-        this.data = data;
-        this.hora = hora;
-        this.tipoConsulta = tipoConsulta;
-        this.numeroConsultorio = numeroConsultorio;
-    }
+	   // Construtor
+	   public AgendaSaude(Medico medico, LocalDate data,
+	   LocalTime hora, String tipoConsulta, int numeroConsultorio, String gravidade) {
+			this.medico = medico;
+	    	this.data = data;
+	    	this.hora = hora;
+	    	this.tipoConsulta = tipoConsulta;
+			this.numeroConsultorio = numeroConsultorio;
+			this.gravidade = gravidade;
+	   }
 
-	   // Getters e setters
+	   // Gets e sets
+	   public Medico getMedico(){
+			return medico;
+	   }
+
 	   public LocalDate getData() {
 	       return data;
 	   }
@@ -40,6 +49,10 @@ public class AgendaSaude extends Medico {
 	       return tipoConsulta;
 	   }
 
+	   public void setMedico(Medico medico){
+			this.medico = medico;
+	   }
+
 	   public void setTipoConsulta(String tipoConsulta) {
 	       this.tipoConsulta = tipoConsulta;
 	   }
@@ -51,4 +64,13 @@ public class AgendaSaude extends Medico {
     public void setNumeroConsultorio(int numeroConsultorio) {
         this.numeroConsultorio = numeroConsultorio;
     }
+
+	public String getGravidade() {
+		return gravidade;
+	
+	}
+
+	public void setGravidade(String gravidade) {
+		this.gravidade = gravidade;
+	}
 	}
